4ba12d2c3080d618194dc4960f8cc19a84cf09be
yuchen112 <andy13579222@gmail.com>
2026-08-23 21:50:42 +0800

    feat(cxq): animate Sadness with dedicated walking frames

--- games/cxq-fairytale-richman/core.js
+++ games/cxq-fairytale-richman/core.js
@@ -19,7 +19,7 @@ const SAVE='cxq_richman_latest_save_v4',PREF='cxq_richman_pref_v1';
 const S={scene:'home',buttons:[],seats:[{type:'human',char:6,diff:'standard'},{type:'ai',char:1,diff:'standard'},{type:'off',char:2,diff:'standard'},{type:'off',char:3,diff:'standard'}],activeSeat:0,mapIndex:0,money:200000,rounds:30,victory:'assets',eventLevel:'standard',startingCards:1,gods:true,board:null,msg:'',rolling:false,dice:1,forcedDice:0,pickAnim:null,settings:{master:80,bgm:70,sfx:80,vibrate:true,lang:'zh-Hant',graphics:'medium'}};
 try{Object.assign(S.settings,JSON.parse(localStorage.getItem(PREF)||'{}'))}catch(e){}
 
-const ASSET_REV='20260824-0610';
+const ASSET_REV='20260824-0635';
 function load(k,u){const i=new Image();i.decoding='async';i.onload=()=>{IM[k]=i};i.onerror=()=>{IM[k]=null};i.src=u+'?v='+ASSET_REV;IM[k]=i;return i}
 load('btnBlue',A+'ui/btn_blue.webp');load('btnRed',A+'ui/btn_red.webp');
 load('homeMenuNew',A+'ui/home_menu_new_v1.png');load('homeMenuContinue',A+'ui/home_menu_continue_v1.png');load('homeMenuHelp',A+'ui/home_menu_help_v1.png');load('homeMenuSettings',A+'ui/home_menu_settings_v1.png');
@@ -36,6 +36,7 @@ CHAR_KEYS.forEach((k,i)=>{load('c'+i,'../../assets/characters/cxq-role-'+k+'.web
 load('joyWalkRightContact',A+'characters/joy/walk_right_contact_v1.png');load('joyWalkRightPassing',A+'characters/joy/walk_right_passing_v1.png');
 load('dreamWalkRightContact',A+'characters/dream/walk_right_contact_v1.png');load('dreamWalkRightPassing',A+'characters/dream/walk_right_passing_v1.png');
 load('nightWalkRightContact',A+'characters/night/walk_right_contact_v1.png');load('nightWalkRightPassing',A+'characters/night/walk_right_passing_v1.png');
+load('sadnessWalkRightContact',A+'characters/sadness/walk_right_contact_v1.png');load('sadnessWalkRightPassing',A+'characters/sadness/walk_right_passing_v1.png');
 IM.house1=IM.house2=IM.house3=null;
 
 const VIEW={scale:1,ox:0,oy:0};
