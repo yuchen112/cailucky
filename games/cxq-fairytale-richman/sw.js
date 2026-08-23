78a5230f5cf1c5740ce961a668e387797175dc05
yuchen112 <andy13579222@gmail.com>
2026-08-23 22:32:40 +0800

    feat(cxq): add full match results and rematch flow

--- games/cxq-fairytale-richman/sw.js
+++ games/cxq-fairytale-richman/sw.js
@@ -1,4 +1,4 @@
-const CACHE='cxq-richman-20260824-0945';
+const CACHE='cxq-richman-20260824-1020';
 const PRECACHE=[
   './','./index.html','./core.js','./viewport.js','./game.js','./manifest.webmanifest',
   './assets/backgrounds/home_scene_v7.png','./assets/backgrounds/setup_scene_v4.png',
